5a51b38fc8e0b707e9b79701d6ac69b45e39b29c
Francisco-Alamino-Neto <franciscoalaminoneto26@gmail.com>
2025-06-02 09:23:41 -0300

    rename ex8.java to Ex8.java

--- Ex8.java
+++ Ex8.java
@@ -3,7 +3,7 @@ package exercicios;
 import java.util.Scanner;
 import java.util.Random;
 
-public class ex8 {
+public class Ex8 {
 
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
