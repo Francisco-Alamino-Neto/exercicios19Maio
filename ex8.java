ef1579572820208519be3739afb60d39fd00d2bc
Francisco-Alamino-Neto <franciscoalaminoneto26@gmail.com>
2025-06-02 08:27:11 -0300

    Update ex8.java

--- ex8.java
+++ ex8.java
@@ -10,19 +10,19 @@ public class ex8 {
         Random random = new Random();
         
         double numero1, numero2, resultadoCorreto, resultadoErrado;
-        
+        // Calculadora
         System.out.println("=== Calculadora de Cálculos Matematicamente Errados ===");
         System.out.println("Insira o primeiro número: ");
         numero1 = sc.nextDouble();
-        
+        // Usuário pode inserir o primeiro número
         System.out.println(numero1 + " foi selecionado");
         System.out.println("Insira o operador (+, -, *, /): ");
         char operador = sc.next().charAt(0);
-        
+        // Usário pode inserir operador
         System.out.println(numero1 + " " + operador);
         System.out.println("Insira o segundo número: ");
         numero2 = sc.nextDouble();
-        
+        // Usuário pode inserir o segundo número
         switch (operador) {
         case '+':
         	resultadoCorreto = numero1 + numero2;
@@ -41,7 +41,7 @@ public class ex8 {
             sc.close();
             return;
         }
-        double erro = 1 + (random.nextDouble() * 0.01);
+        double erro = 1 + (random.nextDouble() * 0.01); //após o calculo, adiciona um valor aleatório até 1% acima do resultado original
         resultadoErrado = resultadoCorreto * erro;
         
         System.out.printf(numero1 + " " + operador + " " + numero2 + " = %.4f\n", resultadoErrado);
